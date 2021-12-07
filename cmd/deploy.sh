390a76ab827b5931559e9c72f3f48cf98424cde0
William Thibault <william.thblt@gmail.com>
2021-12-06 22:15:42 -0500

    fix gA

--- cmd/deploy.sh
+++ cmd/deploy.sh
@@ -5,13 +5,19 @@ set -e
 # Status message
 printf "\033[0;32mDeploying updates to GitHub…\033[0m\n" 
 
-# Build the project. 
-hugo
-
 # Go To Public folder 
 cd public 
 
+git checkout -- .
+
 git checkout main
+
+cd ..
+
+# Build the project. 
+hugo
+
+cd public
 
 # Add changes to git. 
 git add .
